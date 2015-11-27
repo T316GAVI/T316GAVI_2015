6ca104cb93fd66b21cdd1ee53861e569ed737048
Eyjolfur Ingi Asgeirsson <eyjolfur.asgeirsson@gmail.com>
2015-11-27 10:01:23 +0000

    autocommit

--- 01_printing.py
+++ 01_printing.py
@@ -12,3 +12,6 @@ a = 'A'
 b = 42
 c = 'Nothing'
 print("Nr 1: {}, Nr 2: {}, Nr 3: {}".format(a,b,c))
+
+print('{3} {1} {0} {2}'.format('a','b','c','d'))
+print('{3} {3} {0} {3}'.format('a','b','c','d'))
