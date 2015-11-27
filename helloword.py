a1912f3ce79b58794939ba293a949316447dc090
Eyjolfur Ingi Asgeirsson <eyjolfur.asgeirsson@gmail.com>
2015-11-27 11:07:47 +0000

    autocommit

--- helloword.py
+++ helloword.py
@@ -1,2 +1,16 @@
 
 print("Hello world")
+
+for i in [1,2,3,4,5]:
+    print(i)
+for i in range(10):
+    print(i,end=' ')
+for i in range(4,10):
+    print(i,end=' ')
+a = 20
+for x in range(a):
+    print(x,end=' ')
+for i in "testing":
+    print(i,end=' ')
+for i in ['hello',24,'?',42]:
+    print(i)
