b892f4b90d85eaba3ab641a95462b90c88e25764
Eyjolfur Ingi Asgeirsson <eyjolfur.asgeirsson@gmail.com>
2015-11-27 09:53:14 +0000

    autocommit

--- 01_printing.py
+++ 01_printing.py
@@ -1,5 +1,7 @@
 print("Hello world")
 print('Hello world')
 
-print("Hello!" + "Goodbye")
+print("Hello!" + "Goodbye", end="!")
 print("Hello!","Goodbye")
+
+print("Hello!", "Goodbye", "The",'End',sep="<--X-->")
