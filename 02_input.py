7145d0c99688abac3a3e1c6f45b4e6603913a2ee
Eyjolfur Ingi Asgeirsson <eyjolfur.asgeirsson@gmail.com>
2015-11-27 10:51:38 +0000

    autocommit

--- 02_input.py
+++ 02_input.py
@@ -9,8 +9,10 @@ print("Year is {} and pi = {}".format(year,pi))
 
 if year < 2015:
     print("{} is not correct")
+    print("you are off by {}".format(2015-year))
+    print("thank you!")
 elif year > 2015:
     print("The future!")
-else
+else:
     print('ok')
 
