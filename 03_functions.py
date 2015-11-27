f4cd8532bb1083d2060b8f0d8646e19ec0ffdd8c
Eyjolfur Ingi Asgeirsson <eyjolfur.asgeirsson@gmail.com>
2015-11-27 11:25:54 +0000

    autocommit

--- 03_functions.py
+++ 03_functions.py
@@ -3,8 +3,16 @@ def message():
     print("This is a message")
     print("in a simple function")
 
+def sum(a,b):
+    result = a + b
+    return result
+
 
 print('hi!')
 message()
 
+x = int(input("First number: "))
+y = int(input("Second number: "))
 
+z = sum(x,y)
+print(z)
