07678ffafe7768b7321a247665b4468a709713d1
Eyjolfur Ingi Asgeirsson <eyjolfur.asgeirsson@gmail.com>
2015-11-27 10:53:41 +0000

    autocommit

--- 02_input.py
+++ 02_input.py
@@ -8,7 +8,7 @@ print("Hi {}, welcome to gagnavinnsla".format(name))
 print("Year is {} and pi = {}".format(year,pi))
 
 if year < 2015:
-    print("{} is not correct")
+    print("{} is not correct".format(year))
     print("you are off by {}".format(2015-year))
     print("thank you!")
 elif year > 2015:
