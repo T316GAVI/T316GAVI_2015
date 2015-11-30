671c4a1fff42381c29125ca70bc90278bce05b9b
Eyjolfur Ingi Asgeirsson <eyjolfur.asgeirsson@gmail.com>
2015-11-30 10:12:40 +0000

    autocommit

--- 06_fileread.py
+++ 06_fileread.py
@@ -1,11 +1,11 @@
 
-f = open('randomtolur.txt','r')
+f = open('randomnumbers.txt','r')
 
 t = [0]*6
 
 for x in f:   # x is a line in the file f
     print('-----------')
-    type(x)
+    print(type(x))
     print('"{}"'.format(x))
 
 
