5e759fe75e76b82264441017bf55803f0d881c54
Eyjolfur Ingi Asgeirsson <eyjolfur.asgeirsson@gmail.com>
2015-11-30 10:16:42 +0000

    autocommit

--- 06_fileread.py
+++ 06_fileread.py
@@ -4,9 +4,11 @@ f = open('randomnumbers.txt','r')
 t = [0]*6
 
 for x in f:   # x is a line in the file f
-    print('-----------')
-    print(type(x))
-    print('"{}"'.format(x))
+    value = int(x)
 
+    t[value-1] += 1
 
 f.close()
+
+for i in range(6):
+    print('The value {} appears {} times'.format(i+1, t[i]))
