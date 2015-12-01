54e6b9434c7139614b9de3ba11d2238911db5b0a
Eyjolfur Ingi Asgeirsson <eyjolfur.asgeirsson@gmail.com>
2015-12-01 10:14:20 +0000

    autocommit

--- 09_csv_test2.py
+++ 09_csv_test2.py
@@ -1,3 +1,9 @@
 import csv
 
-with open('MAN01405.csv',r) as csvfile:
+with open('MAN01405.csv','r') as csvfile:
+    currentdialect = csv.Sniffer().sniff(csvfile.read(1024))
+    csvfile.seek(0)
+
+    dictreader = csv.DictReader(csvfile, dialect = currentdialect)
+
+
