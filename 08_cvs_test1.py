bba4087b394ddb9c2e047bf76e31bbe318e289af
Eyjolfur Ingi Asgeirsson <eyjolfur.asgeirsson@gmail.com>
2015-12-01 09:44:07 +0000

    autocommit

--- 08_cvs_test1.py
+++ 08_cvs_test1.py
@@ -1,7 +1,7 @@
 import csv
 
 with open('MAN01405.csv') as csvfile:
-    thereader = csv.reader(csvfile)
+    thereader = csv.reader(csvfile, delimiter=';')
 
     for row in thereader:
         print(len(row))
