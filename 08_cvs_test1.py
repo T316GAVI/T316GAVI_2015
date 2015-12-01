3018af69715ebc8fb76be8893092db3bd7cde938
Eyjolfur Ingi Asgeirsson <eyjolfur.asgeirsson@gmail.com>
2015-12-01 09:48:11 +0000

    autocommit

--- 08_cvs_test1.py
+++ 08_cvs_test1.py
@@ -5,8 +5,12 @@ brottfluttir = []
 with open('MAN01405.csv') as csvfile:
     thereader = csv.reader(csvfile, delimiter=';')
 
+    readingheaderrow = True
     for row in thereader:
-        brottfluttir.append(row[5])
+        if readingheaderrow:
+            readingheaderrow = False
+        else:
+            brottfluttir.append(int(row[5]))
 
 print('Total brottfluttir: ', sum(brottfluttir))
 print()
